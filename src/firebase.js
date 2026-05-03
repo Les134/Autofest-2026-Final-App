76d4e52049960ae7a063910ef4fd2d448fe954d0
Les134 <admin@autofest.com.au>
2026-05-03 14:05:41 +1000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "PASTE HERE",
-  authDomain: "PASTE HERE",
-  projectId: "PASTE HERE"
+  apiKey: "PASTE_YOURS",
+  authDomain: "PASTE_YOURS",
+  projectId: "PASTE_YOURS",
+  storageBucket: "PASTE_YOURS",
+  messagingSenderId: "PASTE_YOURS",
+  appId: "PASTE_YOURS"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
